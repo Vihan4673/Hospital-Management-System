50b712f84ad70b5cdc1605fc1e324f501e38d15f
vihan <vihanvimen46@gmail.com>
2026-06-24 13:09:45 +0530

    fixed

--- frontend/src/types/Appointment.ts
+++ frontend/src/types/Appointment.ts
@@ -4,17 +4,12 @@ import type { Patient } from "./Patient.ts";
 export type Appointment = {
   date: string;
   _id: string;
-  appointmentId?: string; // 👈 Auto-generated readable ID එකක් තියෙනවා නම් (Optional)
-
+  appointmentId?: string;
   doctor: Doctor;
   patient: Patient;
-
   appointmentDate: string;
   roomNumber: string;
-
-  // ⚡ FIX: Doctor's Fee එක Appointment type එකට එකතු කළා
   doctorFee: number;
-
   notes?: string | null;
   status: 'pending' | 'completed' | 'cancelled';
   createdAt?: string;
