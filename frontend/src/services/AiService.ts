99fd8d100720c78e7f174739ec1a779bd0ed7691
vihan <vihanvimen46@gmail.com>
2026-06-25 12:42:30 +0530

    fixed

--- frontend/src/services/AiService.ts
+++ frontend/src/services/AiService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/ai";
+const API_URL = "https://hospital-management-system-production-77e5.up.railway.app/api/ai";
 
 export const getAiRecommendation = async (symptoms: string, availableSpecialties: string[]) => {
     const response = await axios.post(`${API_URL}/analyze`, {
